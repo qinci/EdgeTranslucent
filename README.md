1d046b8141899e927603d6617bd4a329bfb98d30
qinci <shenqinci@qq.com>
2017-01-16 22:03:59 +0800

    项目创建、提交

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # EdgeTranslucence
-Android 任意View边沿透明
+Android 任意View边沿渐变透明
+
+####代码简单，使用简单
+
+#####使用
+* 引入库
+	* compile 'ren.qinc.edgetranslucent:lib:0.0.1'
+	
+* 布局
+
+```xml
+     <me.qinc.lib.edgetranslucent.EdgeTransparentView
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            app:edge_width="30dp"
+            app:edge_position="top|bottom">
+            
+            <需要边沿透明的View
+            />
+            
+    </me.qinc.lib.edgetranslucent.EdgeTransparentView>
+```
+
+* 属性说明
+  1. edge_width：边沿透明的宽度（不填写默认20dp）
+  2. edge_position：透明的位置（可以组合使用，不填写默认四个边沿都透明）
+       * top
+       * bottom
+       * left
+       * right
+
+
+* 演示图（不要在意文字内容）ps：有点像阴影，但不是，是渐变透明效果
+
+	![Image](image/image.gif)
+
+* 最后欢迎您star
---
+++ lib/src/main/java/me/qinc/lib/edgetranslucent/EdgeTransparentView.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2017. SHENQINCI(沈钦赐)<946736079@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.qinc.lib.edgetranslucent;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * 任意View边沿透明
+ * Created by shenqinci on 2016/12/3.
+ */
+public class EdgeTransparentView extends FrameLayout {
+    private Paint mPaint;
+    private int position;
+    private float drawSize;
+
+    private int topMask = 0x01;
+    private int bottomMask = topMask << 1;
+    private int leftMask = topMask << 2;
+    private int rightMask = topMask << 3;
+
+    private int mWidth;
+    private int mHeight;
+
+    public EdgeTransparentView(Context context) {
+        this(context, null);
+    }
+
+    public EdgeTransparentView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EdgeTransparentView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+
+
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.EdgeTransparentView);
+        position = typedArray.getInt(R.styleable.EdgeTransparentView_edge_position, 0);
+        drawSize = typedArray.getDimension(R.styleable.EdgeTransparentView_edge_width, Utils.d2p(getContext(), 20));
+        typedArray.recycle();
+    }
+
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        initShader();
+        mWidth = getWidth();
+        mHeight = getHeight();
+    }
+
+    //渐变颜色
+    private int[] mGradientColors = {0xffffffff, 0x00000000};
+    //渐变位置
+    private float[] mGradientPosition = new float[]{0, 1};
+
+    private void initShader() {
+        mPaint.setShader(new LinearGradient(0, 0, 0, drawSize, mGradientColors, mGradientPosition, Shader.TileMode.CLAMP));
+    }
+
+
+    @Override
+    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
+        int layerSave = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        boolean drawChild = super.drawChild(canvas, child, drawingTime);
+        if (position == 0 || (position & topMask) != 0) {
+            canvas.drawRect(0, 0, mWidth, drawSize, mPaint);
+        }
+
+        if (position == 0 || (position & bottomMask) != 0) {
+            int save = canvas.save();
+            canvas.rotate(180, mWidth / 2, mHeight / 2);
+            canvas.drawRect(0, 0, mWidth, drawSize, mPaint);
+            canvas.restoreToCount(save);
+        }
+
+        int offset = (mHeight - mWidth) / 2;
+        if (position == 0 || (position & leftMask) != 0) {
+            int saveCount = canvas.save();
+            canvas.rotate(90, mWidth / 2, mHeight / 2);
+            canvas.translate(0, offset);
+            canvas.drawRect(0 - offset, 0, mWidth + offset, drawSize, mPaint);
+            canvas.restoreToCount(saveCount);
+        }
+
+        if (position == 0 || (position & rightMask) != 0) {
+            int saveCount = canvas.save();
+            canvas.rotate(270, mWidth / 2, mHeight / 2);
+            canvas.translate(0, offset);
+            canvas.drawRect(0 - offset, 0, mWidth + offset, drawSize, mPaint);
+            canvas.restoreToCount(saveCount);
+        }
+
+        canvas.restoreToCount(layerSave);
+        return drawChild;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
